09279f74b8c36b7e55a9defb786f6fba8e0bfb6d
tboffice <tbofficed@gmail.com>
2012-07-28 18:33:34 +0900

    typo修正

--- hn-scala-hwg/img/img_convert.sh
+++ hn-scala-hwg/img/img_convert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 convert new_project.png -type GrayScale eps2:new_project.eps
-convert create_new_scala_class.png -type GrayScalee ps2:create_new_scala_class.eps
+convert create_new_scala_class.png -type GrayScale eps2:create_new_scala_class.eps
 convert intellij.png -type GrayScale eps2:intellij.eps
 convert hello_world_code.png -type GrayScale eps2:hello_world_code.eps
