f829bf694a6b21dc00959a7597a268c887b23293
じゅんいち☆かとう <j5ik2o@gmail.com>
2012-07-28 18:22:25 +0900

    ちょろっと修正

--- hn-scala-hwg/img/img_convert.sh
+++ hn-scala-hwg/img/img_convert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-convert new_project.png eps2:new_project.eps
-convert create_new_scala_class.png eps2:create_new_scala_class.eps
-convert intellij.png eps2:intellij.eps
-convert hello_world_code.png eps2:hello_world_code.eps
+convert new_project.png -type GrayScale eps2:new_project.eps
+convert create_new_scala_class.png -type GrayScalee ps2:create_new_scala_class.eps
+convert intellij.png -type GrayScale eps2:intellij.eps
+convert hello_world_code.png -type GrayScale eps2:hello_world_code.eps
