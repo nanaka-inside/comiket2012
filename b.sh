aa56dee8608ca07666820e8d34ef70d8949af3d8
tboffice <tbofficed@gmail.com>
2012-07-28 14:48:14 +0900

    fix b.sh dvipdfmx options

--- b.sh
+++ b.sh
@@ -10,4 +10,4 @@ platex -shell-escape index.tex
 if ! [ -d ./build-environment ]; then
   mkdir build-environment
 fi
-dvipdfmx -p b5 -c -r 600 -v -V 3 -o build-environment/index.pdf index
+dvipdfmx -c -r 600 -v -V 3 -o build-environment/index.pdf index
