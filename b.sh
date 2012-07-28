520564cb6bbb15fa1abe0e0ed0607019416f86ee
mtgto <hogerappa@gmail.com>
2012-07-28 14:23:45 +0900

    fix build script for first execution.

--- b.sh
+++ b.sh
@@ -7,4 +7,7 @@ platex -shell-escape index.tex
 mendex -f index.idx
 platex -shell-escape index.tex
 
+if ! [ -d ./build-environment ]; then
+  mkdir build-environment
+fi
 dvipdfmx -p b5 -c -r 600 -v -V 3 -o build-environment/index.pdf index
--- d.sh
+++ d.sh
@@ -8,6 +8,9 @@ mendex -f index.idx
 platex -shell-escape index.tex
 dvipdfmx index 
 
+if ! [ -d ./build-environment ]; then
+  mkdir build-environment
+fi
 dvipdfmx -p b5 -c -r 600 -v -V 3 -o build-environment/index.pdf index
 
 find -type f \( -name "*aux" -or -name "*out" -or -name "*toc" -or -name "*ilg" -or -name "*dvi" -or -name "*log" \) -exec rm {} \;
